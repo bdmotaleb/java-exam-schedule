1ba9a8f723d6f24109f40ba2ad04378586831e6d
Motaleb Hossain <bdmotaleb33@gmail.com>
2023-03-09 01:20:56 +0600

    init

---
+++ src/examroutinemanagement/ExamRoutineManagement.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examroutinemanagement;
+
+/**
+ *
+ * @author User
+ */
+public class ExamRoutineManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login log = new Login();
+        
+        log.show();
+    }
+    
+}
